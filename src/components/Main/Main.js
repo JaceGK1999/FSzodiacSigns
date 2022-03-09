49905f3de0b9e1135bb822419f69d13e40514fff
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-08 16:55:57 -0800

    moved assets to different location, added background

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { zodiac } from '../../data.js';
 import ZodiacRender from '../Zodiacs/Zodiacs';
+import './Main.css';
 
 export default function Main() {
   return (
-    <div>
-      test
+    <div className="zodiac-main">
       {zodiac.map((item) => (
         <ZodiacRender key={item.name} {...item} />
       ))}
--- src/components/Zodiacs/Zodiacs.js
+++ src/components/Zodiacs/Zodiacs.js
@@ -1,7 +1,8 @@
 export default function ZodiacRender({ name, dates }) {
+  console.log(name, 'name');
   return (
     <div className="zodiac-render">
-      <img alt={name} src={`../assets/${name}.png`} />
+      <img alt={name} src={`../../../${process.env.PUBLIC_URL}/assets/${name}.png`} />
       <span className="name">{name}</span>
       <span>{dates}</span>
     </div>
